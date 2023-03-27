6730143a2f0498e8ec5c7bfefec4796a56e6fa5b
Ronaldo Pei Piro <ronaldopeipiro11@gmail.com>
2023-03-27 11:30:29 +0700

    1 upload

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Undangan Tunangan || Miekel & TDC</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Undangan Tunangan" />
+	<meta name="keywords" content="undangan, tunangan, miekel, jerianto, miekel jerianto, theresia, dwi clara, dwi, clara, jerry, jeri, web undangan, desain web" />
+	<meta name="author" content="@rnaldppr" />
+
+	<meta property="og:title" content="Undangan Tunangan - Miekel Jerianto & Theresia Dwi Clara" />
+	<meta property="og:image" content="./images/icon.jpg" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:description" content="Website Undangan Tunangan - Miekel Jerianto dan Theresia Dwi Clara" />
+
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link rel="shortcut icon" href="./images/icon.jpg">
+
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Clicker+Script&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+
+<body>
+
+	<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+
+			<div class="fh5co-hero" data-section="home">
+				<div class="fh5co-overlay"></div>
+				<div class="fh5co-cover text-center" style="background-image: url(images/2.jpeg); background-position: center;">
+					<div class="display-t">
+						<div class="display-tc">
+							<div class="container">
+								<div class="col-md-10 col-md-offset-1">
+									<div class="animate-box">
+										<h1 style="font-family: 'Lora', serif; font-size: 30px;">
+											UNDANGAN TUNANGAN
+										</h1>
+										<br>
+										<h2>
+											Theresia Dwi Clara <br>
+											&amp; <br>
+											Miekel Jerianto
+										</h2>
+										<br>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- <header id="fh5co-header-section" class="sticky-banner">
+			<div class="container">
+				<div class="nav-header">
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+					<h1 id="fh5co-logo"><a href="index.html">Nuptial</a></h1>
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li class="active"><a href="index.html" >Home</a></li>
+							<li>
+								<a href="groom-bride.html" class="fh5co-sub-ddown">Groom &amp; Bride</a>
+								<ul class="fh5co-sub-menu">
+									<li><a href="#">Dropdown Menu 1</a></li>
+									<li><a href="#">Dropdown Menu 2</a></li>
+									<li><a href="#">Dropdown Menu 3</a></li>
+									<li><a href="#">Dropdown Menu 4</a></li>
+								</ul>
+							</li>
+							<li><a href="when-where.html">When &amp; Where</a></li>
+							<li><a href="guest.html">Guest</a></li>
+							<li><a href="gallery.html">Gallery</a></li>
+							<li><a href="blog.html">Blog</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header> -->
+
+			<!-- end:header-top -->
+
+			<div id="fh5co-couple" class="fh5co-section-gray">
+				<div class="container">
+					<div class="row animate-box">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<div class="col-md-5 col-sm-5 col-xs-5 nopadding">
+								<img src="images/11.png" class="img-responsive" alt="">
+								<h5 class="text-center" style="font-weight: 600; font-family: 'Lora', serif;">
+									Miekel Jerianto, S.Kom.
+								</h5>
+							</div>
+							<div class="col-md-2 col-sm-2 col-xs-2 nopadding">
+								<h2 class="amp-center"><i class="icon-heart"></i></h2>
+							</div>
+							<div class="col-md-5 col-sm-5 col-xs-5 nopadding">
+								<img src="images/12.png" class="img-responsive" alt="">
+								<h5 class="text-center" style="font-weight: 600; font-family: 'Lora', serif;">
+									Theresia Dwi Clara, S.Pd.
+								</h5>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div id="fh5co-started" style="background-image:url(images/cincin.jpg); background-repeat: no-repeat; background-position: bottom; background-size: cover;">
+				<div class="container">
+					<div class="overlay"></div>
+					<div class="row animate-box">
+						<div class="col-md-8 col-md-offset-2">
+							<div class="col-md-12 text-center heading-section">
+								<h2 style="font-weight: 800;">Are Getting Engaged</h2>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-lg-8 col-lg-offset-2">
+							<div class="row animate-box">
+								<div class="col-lg-5 col-md-4 col-sm-4 col-xs-4 text-center">
+									<hr style="height: 2px; background-color: #fff;">
+									<span style="color: #fff; font-weight: 900; font-size: 14px;">
+										SABTU
+									</span>
+									<hr style="height: 2px; background-color: #fff;">
+								</div>
+								<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 text-center">
+									<h2 style="color: #fff; font-weight: 900; font-size: 40px; margin: 0;">
+										08
+									</h2>
+									<span style="color: #fff; font-weight: 900; font-size: 18px;">
+										OKTOBER
+									</span>
+									<br>
+									<h2 style="color: #fff; font-weight: 900; font-size: 40px; margin: 8px 0;">
+										2022
+									</h2>
+								</div>
+								<div class="col-lg-5 col-md-4 col-sm-4 col-xs-4 text-center">
+									<hr style="height: 2px; background-color: #fff;">
+									<span style="color: #fff; font-weight: 900; font-size: 14px;">
+										18.30 - Selesai
+									</span>
+									<hr style="height: 2px; background-color: #fff;">
+								</div>
+							</div>
+
+							<div class="row animate-box">
+								<div class="col-lg-12">
+									<br>
+									<br>
+									<p class="text-center" style="color: #fff; font-size: 15px;">
+										<strong>
+											Bertempat di Kediaman Bapak Remigius & Ibu Rita <br>
+											Simpang Dua, Ketapang, Kalimantan Barat
+										</strong>
+									</p>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+					<div id="fh5co-countdown" class="row animate-box">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<p class="countdown">
+								<span id="demo"></span>
+								<span style="font-family: 'Lora', serif; " id="days"></span>
+								<span style="font-family: 'Lora', serif; " id="hours"></span>
+								<span style="font-family: 'Lora', serif; " id="minutes"></span>
+								<span style="font-family: 'Lora', serif; " id="seconds"></span>
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="fh5co-section-gray animate-box">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<br>
+							<br>
+							<h1 class="text-center font-weight-bold" style="font-family: 'Lora', serif; ">
+								Peta Lokasi
+							</h1>
+							<hr style="height: 2px; background-color: #333;">
+
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7925.55298745902!2d110.35040251623552!3d-0.8054774543813229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2a59f8d94dd62561!2zMMKwNDgnMTguNyJTIDExMMKwMjEnMTUuOCJF!5e0!3m2!1sid!2sid!4v1663664190276!5m2!1sid!2sid" width="100%" height="500px" style="border:0; border-radius: 30px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+							<br>
+							<br>
+							<div class="text-center">
+								<a href="https://google.co.id/maps?q=-0.805213,110.354400" class="btn btn-info" style="background-color: #ff68b4; color: #fff;" target="_blank">
+									Petunjuk Arah
+								</a>
+							</div>
+							<br>
+							<br>
+							<br>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="fh5co-section-gray animate-box " style="background-image:url(images/ring.jpg); background-repeat: no-repeat; background-position: bottom; background-size: cover;">
+				<div class="overlay"></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-12 text-center" style="font-family: 'Lora', serif; color: #fff;">
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<span>
+								Tanpa mengurangi rasa hormat, <br> kepada setiap tamu undangan yang akan hadir wajib mematuhi protokol kesehatan yang dianjurkan oleh pemerintah.
+							</span>
+							<br>
+							<br>
+							<span>
+								Pastikan kondisi tubuh anda dalam keadaan sehat saat hadir di acara.
+							</span>
+							<br>
+							<span>
+								Terima kasih atas partisipasi Bapak/Ibu/Saudara/Saudari
+							</span>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+							<br>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+		<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<!-- jQuery -->
+	<script src="dist/scripts.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
